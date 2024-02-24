53a26131999f2bfd3fb8c500e84ed54a8ec92096
GabeGibb <97437160+GabeGibb@users.noreply.github.com>
2024-02-23 22:07:02 -0800

    formatted output and gave some debug feedback if a word is not in index

--- A3_M2.py
+++ A3_M2.py
@@ -8,21 +8,28 @@ def load_index():
 # Searching Query
 def search_index(index):
     print()
-    query = input("Search Bar: ").split(" ")
-    try:
-        relevancy_dict = {}
-        for word in query:
-            result = sorted(index[word].items(), key=lambda x: x[1], reverse=True)[:5]
-            for pair in result:
-                if pair[0] not in relevancy_dict:
-                    relevancy_dict[pair[0]] = pair[1]
-                else:
-                    relevancy_dict[pair[0]] += pair[1]
+    query = input("Search Bar: ").split()
+    # try:
+    relevancy_dict = {}
+    for word in query:
+        if word not in index: # if word not in index, skip it
+            print(word, "not found in index")
+            continue
+        
+        result = sorted(index[word].items(), key=lambda x: x[1], reverse=True)[:20] #changed to 20 values to be a little more accurate
+        for pair in result:
+            if pair[0] not in relevancy_dict:
+                relevancy_dict[pair[0]] = pair[1]
+            else:
+                relevancy_dict[pair[0]] += pair[1]
 
-        top_5 = sorted(relevancy_dict.items(), key=lambda x: x[1], reverse=True)[:5]
-        print(top_5)
-    except:
-        print("Error")
+    top_5 = sorted(relevancy_dict.items(), key=lambda x: x[1], reverse=True)[:5]
+    # print(top_5)
+    print('TOP URLS:')
+    for top in top_5:
+        print(top[0])
+    print()
+
 
 if __name__ == "__main__":
     search_index(load_index())
